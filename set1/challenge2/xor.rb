d7020bb43ba5fdf46989935a4af4eed7e5aedada
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-14 00:02:50 +0530

    Fix xor methods

--- set1/challenge2/xor.rb
+++ set1/challenge2/xor.rb
@@ -1,9 +1,13 @@
 require_relative "../challenge1/conversion.rb"
 
-def xor(hex_string_first, hex_string_second)
+def xor_hex_strings(hex_string_first, hex_string_second)
   binary_string_first = encode_to_binary(decode_from_hex(hex_string_first))
   binary_string_second = encode_to_binary(decode_from_hex(hex_string_second))
 
+  xor_binary_strings(binary_string_first, binary_string_second)
+end
+
+def xor_binary_strings(binary_string_first, binary_string_second)
   zipped_binary_array = binary_string_first.split('').zip binary_string_second.split('')
   result_binary_string = zipped_binary_array.map do |chr1, chr2|
     (chr1.to_i ^ chr2.to_i).to_s
