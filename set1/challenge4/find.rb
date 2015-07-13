b2def9326584b2928b8fbcef595711ba0f8e0230
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-14 01:16:47 +0530

    Fix solution for set1 challenge4

--- set1/challenge4/find.rb
+++ set1/challenge4/find.rb
@@ -3,6 +3,6 @@ require_relative("../challenge3/single_byte_xor.rb")
 def detect_single_character_xor_from_file filename
   file = File.open(filename, "r")
   file.each_line do |line|
-    print_probable_single_byte_xor_keys_for_cipher line
+    puts probable_single_byte_xor_key_for_hex_cipher line
   end
 end
