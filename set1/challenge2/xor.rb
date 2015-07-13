605bb900c3dfb2d2a9c1fbcc64cbb0477a32eee9
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-14 01:02:45 +0530

    Fix xoring return values

--- set1/challenge2/xor.rb
+++ set1/challenge2/xor.rb
@@ -4,14 +4,13 @@ def xor_hex_strings(hex_string_first, hex_string_second)
   binary_string_first = encode_to_binary(decode_from_hex(hex_string_first))
   binary_string_second = encode_to_binary(decode_from_hex(hex_string_second))
 
-  xor_binary_strings(binary_string_first, binary_string_second)
+  result_binary_string = xor_binary_strings(binary_string_first, binary_string_second)
+  encode_to_hex(decode_from_binary(result_binary_string))
 end
 
 def xor_binary_strings(binary_string_first, binary_string_second)
   zipped_binary_array = binary_string_first.split('').zip binary_string_second.split('')
-  result_binary_string = zipped_binary_array.map do |chr1, chr2|
+  zipped_binary_array.map do |chr1, chr2|
     (chr1.to_i ^ chr2.to_i).to_s
   end.join
-  
-  encode_to_hex(decode_from_binary(result_binary_string))
 end
