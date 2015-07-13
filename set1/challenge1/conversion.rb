33f77d3c7fc4fbc8c2cc1b792095de4b57b6f06d
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-13 23:23:58 +0530

    Refactor set1 challenge1

--- set1/challenge1/conversion.rb
+++ set1/challenge1/conversion.rb
@@ -1,12 +1,31 @@
-def hex_to_base64(string)
-  encode_base64 [string].pack("H*")
+def hex_to_base64(hex_string)
+  encode_to_base64(decode_from_hex(hex_string)) 
 end
 
-def encode_base64(string)
+# base64
+def encode_to_base64(string)
   [string].pack("m0")
 end
 
-def decode_base64(string)
-  string.unpack("m0")
+def decode_from_base64(base64_string)
+  base64_string.unpack("m0")
+end
+
+# hexadecimal
+def encode_to_hex(string)
+  string.unpack("H*")
+end
+
+def decode_from_hex(hex_string)
+  [hex_string].pack("H*")
+end
+
+# binary
+def encode_to_binary(string)
+  string.unpack("B*")
+end
+
+def decode_from_binary(binary_string)
+  [binary_string].pack("B*")
 end
 
