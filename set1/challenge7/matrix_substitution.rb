df8119d2f86d0026affc12f6d2c7d103fe332738
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-14 23:28:14 +0530

    Fix matrix substitutions

--- set1/challenge7/matrix_substitution.rb
+++ set1/challenge7/matrix_substitution.rb
@@ -1,12 +1,10 @@
 require "./matrix.rb"
 require "./substitution.rb"
 
-include Matrix
-
-module Substitution
+module Matrix
   def self.get_matrix_128_after_substitution(matrix_128)
     substituted_bytes = matrix_128.get_bytes.map do |byte|
-      sub_byte byte
+      Substitution.sub_byte byte
     end
     Matrix_128.new(substituted_bytes)
   end
