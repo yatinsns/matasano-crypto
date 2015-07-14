3b3e240c8bbe5e286053bc94e54dd1345ac466aa
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-14 22:53:22 +0530

    Fix substitution methods

--- set1/challenge7/spec/substitution_spec.rb
+++ set1/challenge7/spec/substitution_spec.rb
@@ -2,10 +2,10 @@ require_relative "../substitution.rb"
 
 describe "Substitution" do
   it "should get correct sub byte for given input" do
-    Substitution.sub_byte(248).should eql "A"
+    Substitution.sub_byte(248).should eql 65
   end
 
   it "should get correct inverse sub byte for given input" do
-    Substitution.sub_byte_inverse(160).should eql "G"
+    Substitution.sub_byte_inverse(160).should eql 71
   end
 end
--- set1/challenge7/substitution.rb
+++ set1/challenge7/substitution.rb
@@ -3,10 +3,10 @@ require "../challenge1/conversion.rb"
 
 module Substitution
   def self.sub_byte(byte)
-    decode_from_hex s_box[(byte & 0xf0) >> 4][byte & 0x0f]
+    s_box[(byte & 0xf0) >> 4][byte & 0x0f].to_i 16
   end
 
   def self.sub_byte_inverse(byte)
-    decode_from_hex s_box_inverse[(byte & 0xf0) >> 4][byte & 0x0f]
+    s_box_inverse[(byte & 0xf0) >> 4][byte & 0x0f].to_i 16
   end
 end
