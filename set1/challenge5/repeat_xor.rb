05158e5d1f1f1a280995de6b4331982060db8db4
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-14 15:41:16 +0530

    Fix solution for set1 challenge5

--- set1/challenge5/repeat_xor.rb
+++ set1/challenge5/repeat_xor.rb
@@ -1,6 +1,15 @@
-require_relative "../challenge3/single_byte_xor.rb"
+require_relative "../challenge3/string_additions.rb"
+require_relative "../challenge2/xor.rb"
+require_relative "../challenge1/conversion.rb"
 
-def encrypt_string_using_repeat_xor_key(string, xor_key_string)
-  full_xor_key_string = repeat_xor_key_for_length(xor_key_string, string.length)
-  xor(string.unpack("H*").first, full_xor_key_string.unpack("H*").first)
+def encrypt_string_using_repeat_xor_key(string, xor_key)
+  binary_string = encode_to_binary string
+  binary_xor_key = encode_to_binary xor_key
+  result_binary_string = encrypt_binary_string_using_repeat_xor_binary_key(binary_string, binary_xor_key)
+  encode_to_hex(decode_from_binary(result_binary_string))
+end
+
+def encrypt_binary_string_using_repeat_xor_binary_key(binary_string, xor_binary_key)
+  full_xor_binary_key = xor_binary_key.repeat_for_length binary_string.length
+  xor_binary_strings(binary_string, full_xor_binary_key)
 end
---
+++ set1/challenge5/repeat_xor_spec.rb
@@ -0,0 +1,12 @@
+require "./repeat_xor.rb"
+
+describe "Repeat xor" do
+  describe "matsano crypto set1 challenge5" do
+    it "should test given input" do
+      string = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
+      key = "ICE"
+      result = encrypt_string_using_repeat_xor_key(string, key)
+      result.should eql "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    end
+  end
+end
