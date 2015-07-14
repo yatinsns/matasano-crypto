05d6affa0166d128c40bfa3ff8ffe6ffc9fb5592
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-15 00:32:53 +0530

    Fix matrix substitution

--- set1/challenge7/matrix_substitution.rb
+++ set1/challenge7/matrix_substitution.rb
@@ -2,11 +2,12 @@ require "./matrix.rb"
 require "./substitution.rb"
 
 module Matrix
-  def self.get_matrix_128_after_substitution(matrix_128)
-    matrix_128.set_bytes(matrix_128.get_bytes.map do |byte|
-      Substitution.sub_byte byte
-    end)
-    matrix_128
+  class Matrix_128
+    def substitute
+      self.set_bytes(self.get_bytes.map do |byte|
+        Substitution.sub_byte byte
+      end)
+    end
   end
 end
 
--- set1/challenge7/spec/matrix_substitution_spec.rb
+++ set1/challenge7/spec/matrix_substitution_spec.rb
@@ -3,11 +3,11 @@ require_relative "../matrix_substitution.rb"
 describe "Matrix Substitution" do
   it "should substitute matrix_128" do
     matrix = Matrix_128.new([25, 61, 227, 190, 160, 244, 226, 43, 154, 198, 141, 42, 233, 248, 72, 8])
-    new_matrix = Matrix.get_matrix_128_after_substitution matrix
+    matrix.substitute
 
-    new_matrix.get_row(0).should eql [212, 224, 184, 30]
-    new_matrix.get_row(1).should eql [39, 191, 180, 65]
-    new_matrix.get_row(2).should eql [17, 152, 93, 82]
-    new_matrix.get_row(3).should eql [174, 241, 229, 48]
+    matrix.get_row(0).should eql [212, 224, 184, 30]
+    matrix.get_row(1).should eql [39, 191, 180, 65]
+    matrix.get_row(2).should eql [17, 152, 93, 82]
+    matrix.get_row(3).should eql [174, 241, 229, 48]
   end
 end
