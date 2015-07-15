17aa0a81368302c806b0f866f25f6b8dd05060c5
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-16 04:50:10 +0530

    Create method to add right padding to strings

--- set1/challenge3/string_additions.rb
+++ set1/challenge3/string_additions.rb
@@ -3,6 +3,10 @@ class String
     ("%#{length}.#{length}s" % self).gsub(' ', padding_chr)
   end
 
+  def fixed_width_length_with_right_padding(length, padding_chr)
+    ("%-#{length}.#{length}s" % self).gsub(' ', padding_chr)
+  end
+
   def repeat_for_length(length)
     Array.new((length / self.length) + 1, self).join.slice(0, length)
   end
