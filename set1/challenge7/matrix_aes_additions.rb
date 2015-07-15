cb08cba9e5fd27e3a66137446793d54db9c42f92
Yatin Sarbalia <yatinsns@gmail.com>
2015-07-16 04:44:50 +0530

    Raise exception

--- set1/challenge7/matrix_aes_additions.rb
+++ set1/challenge7/matrix_aes_additions.rb
@@ -3,6 +3,8 @@ require "./matrix.rb"
 module Matrix
   class Matrix_128
     def self.new_with_string(string)
+      raise "length of string is not 16" unless string.length == 16
+
       bytes = encode_to_binary(string).scan(/.{8}/).map do |str|
         str.to_i 2
       end
