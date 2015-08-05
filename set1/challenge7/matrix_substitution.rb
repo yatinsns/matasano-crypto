75b942710f027819f72c870d7af62bed50e50588
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-06 01:39:51 +0530

    Add matrix inverse substitution

--- set1/challenge7/matrix_substitution.rb
+++ set1/challenge7/matrix_substitution.rb
@@ -5,8 +5,14 @@ module Matrix
   class Matrix_128
     def substitute
       self.set_bytes(self.get_bytes.map do |byte|
-        Substitution.sub_byte byte
-      end)
+                       Substitution.sub_byte byte
+                     end)
+    end
+
+    def inverse_substitute
+      self.set_bytes(self.get_bytes.map do |byte|
+                       Substitution.sub_byte_inverse byte
+                     end)
     end
   end
 end
--- set1/challenge7/spec/matrix_substitution_spec.rb
+++ set1/challenge7/spec/matrix_substitution_spec.rb
@@ -10,4 +10,14 @@ describe "Matrix Substitution" do
     matrix.get_row(2).should eql [17, 152, 93, 82]
     matrix.get_row(3).should eql [174, 241, 229, 48]
   end
+
+  it "should inverse substitute matrix_128" do
+    matrix = Matrix_128.new([212, 39, 17, 174, 224, 191, 152, 241, 184, 180, 93, 229, 30, 65, 82, 48])
+    matrix.inverse_substitute
+
+    matrix.get_row(0).should eql [25, 160, 154, 233]
+    matrix.get_row(1).should eql [61, 244, 198, 248]
+    matrix.get_row(2).should eql [227, 226, 141, 72]
+    matrix.get_row(3).should eql [190, 43, 42, 8]
+  end
 end
