b66f09c30cb14307fd9fd27b5ef1460ab9c1d235
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-07 12:25:26 +0530

    Implement inverse matrix shift rows

--- set1/challenge7/matrix_shift_rows.rb
+++ set1/challenge7/matrix_shift_rows.rb
@@ -4,9 +4,17 @@ require "./shift_row.rb"
 module Matrix
   class Matrix_128
     def shift_row
-      ROW_COUNT.times.map do |row_number|
+      ROW_COUNT.times.each do |row_number|
         shifted_row = ShiftRow.shift_row(self.get_row(row_number), row_number)
         self.set_row(row_number, shifted_row) 
+      end
+    end
+
+    def inverse_shift_row
+      ROW_COUNT.times.each do |row_number|
+        count = row_number == 0 ? 0 : (ROW_COUNT - row_number)
+        shifted_row = ShiftRow.shift_row(self.get_row(row_number), count)
+        self.set_row(row_number, shifted_row)
       end
     end
   end
--- set1/challenge7/spec/matrix_shift_rows_spec.rb
+++ set1/challenge7/spec/matrix_shift_rows_spec.rb
@@ -10,4 +10,14 @@ describe "Matrix Shift Rows" do
     matrix.get_row(2).should eql [93, 82, 17, 152]
     matrix.get_row(3).should eql [48, 174, 241, 229]
   end
+
+  it "should inverse shift row matrix_128" do
+    matrix = Matrix_128.new([212, 39, 17, 174, 224, 191, 152, 241, 184, 180, 93, 229, 30, 65, 82, 48])
+    matrix.inverse_shift_row
+
+    matrix.get_row(0).should eql [212, 224, 184, 30]
+    matrix.get_row(1).should eql [65, 39, 191, 180]
+    matrix.get_row(2).should eql [93, 82, 17, 152]
+    matrix.get_row(3).should eql [241, 229, 48, 174]
+  end
 end
