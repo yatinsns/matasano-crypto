3e51af3a107bb1801ec30f796a2b5152daceb7e3
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-08 21:59:59 +0530

    Fix aes tests

--- set1/challenge7/spec/aes_spec.rb
+++ set1/challenge7/spec/aes_spec.rb
@@ -2,12 +2,12 @@ require_relative "../aes.rb"
 
 describe "AES" do
   it "should encrypt 128 bits in ECB mode" do
-    base64_encoding = AES.encrypt_128_ecb("abcdefghijklmnopqrstuvwxyz", "YELLOW SUBMARINE")
-    base64_encoding.should eql "vbGE1E4fwdMGCUW1PJlPSCEtYythlaDLlReyIuaEieo="
+    base64_encoding = AES.encrypt_128_ecb("abcdefghijklmnop", "YELLOW SUBMARINE")
+    base64_encoding.should eql "vbGE1E4fwdMGCUW1PJlPSA=="
   end
 
   it "should decrypt 128 bits in ECB mode" do
-    base64_encoding = AES.decrypt_128_ecb("CjLau5wzO7F+j4g/LK0UMQ==", "YELLOW SUBMARINE")
-    base64_encoding.should eql "QVRUQUNLIEFUIERBV04hIQ=="
+    base64_encoding = AES.decrypt_128_ecb("vbGE1E4fwdMGCUW1PJlPSA==", "YELLOW SUBMARINE")
+    base64_encoding.should eql "YWJjZGVmZ2hpamtsbW5vcA=="
    end
 end
