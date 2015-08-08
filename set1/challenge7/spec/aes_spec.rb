59bf49d2ad897b1ca6ad0dc0ee38274c196304e6
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-08 21:23:14 +0530

    Add test for aes decryption

--- set1/challenge7/spec/aes_spec.rb
+++ set1/challenge7/spec/aes_spec.rb
@@ -5,4 +5,9 @@ describe "AES" do
     base64_encoding = AES.encrypt_128_ecb("abcdefghijklmnopqrstuvwxyz", "YELLOW SUBMARINE")
     base64_encoding.should eql "vbGE1E4fwdMGCUW1PJlPSCEtYythlaDLlReyIuaEieo="
   end
+
+  it "should decrypt 128 bits in ECB mode" do
+    base64_encoding = AES.decrypt_128_ecb("CjLau5wzO7F+j4g/LK0UMQ==", "YELLOW SUBMARINE")
+    base64_encoding.should eql "QVRUQUNLIEFUIERBV04hIQ=="
+   end
 end
