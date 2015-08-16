f301dc91ab7e567bf00d4009ffbde7d62694836e
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-16 16:47:51 +0530

    Fix aes decryption with padding

--- set1/challenge7/aes_128_ecb.rb
+++ set1/challenge7/aes_128_ecb.rb
@@ -23,31 +23,30 @@ module AES
   end
 
   def self.decrypt_128_ecb(message_base64, cipher_key, is_padded)
-    bytes = encode_to_binary(decode_from_base64(message_base64)).scan(/.{8}/).map do |byte_string|
+    binary_string = encode_to_binary(decode_from_base64(message_base64))
+    bytes = binary_string.scan(/.{8}/).map do |byte_string|
       byte_string.to_i 2
     end
+    
+    result_bytes = bytes.each_slice(16).map do |sliced_bytes|
+      self.decrypt_128_ecb_partial(sliced_bytes, cipher_key)
+    end.flatten
 
-    bytes = PKCS7::pkcs7_padding_remove_bytes bytes if is_padded
+    result_bytes = PKCS7::pkcs7_padding_remove_bytes(result_bytes) if is_padded
 
-    result_binary = bytes.each_slice(16).map do |slice|
-      message = slice.map do |num|
-        num.chr
-      end.join
-      self.decrypt_128_ecb_partial(message, cipher_key)
+    result_binary = result_bytes.map do |i|
+      i.to_s(2).fixed_width_length_with_left_padding(8, "0")
     end.join
 
     encode_to_base64(decode_from_binary(result_binary))
   end
 
-  def self.decrypt_128_ecb_partial(message, cipher_key)
-    state_matrix_128 = Matrix_128.new_with_string(message)
+  def self.decrypt_128_ecb_partial(message_bytes, cipher_key)
+    state_matrix_128 = Matrix_128.new(message_bytes)
     cipher_matrix_128 = Matrix_128.new_with_string(cipher_key)
 
     result_matrix_128 = block_cipher_decryption(state_matrix_128,
                                                 cipher_matrix_128)
-
-    result_matrix_128.get_bytes.map do |i|
-      i.to_s(2).fixed_width_length_with_left_padding(8, "0")
-    end.join
+    result_matrix_128.get_bytes
   end
 end
