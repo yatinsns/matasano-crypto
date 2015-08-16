cabb3a776b3f433e9ec65808bcd7a980a0e796b1
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-16 15:01:53 +0530

    Add method to add and remove PKCS#7 padding

--- set2/challenge9/pkcs7.rb
+++ set2/challenge9/pkcs7.rb
@@ -1,5 +1,10 @@
-def pkcs7_padding(message, block_size)
+def pkcs7_padding_add(message, block_size)
   padding_length = block_size - (message.length % block_size)
   message.ljust(message.length + padding_length,
                 padding_length.chr)
+end
+
+def pkcs7_padding_remove(message, block_size)
+  padding_length = message[message.length - 1].ord
+  message.slice(0, message.length - padding_length)
 end
--- set2/challenge9/spec/pkcs7_spec.rb
+++ set2/challenge9/spec/pkcs7_spec.rb
@@ -3,11 +3,21 @@ require_relative "../pkcs7.rb"
 describe "pkcs7" do
   it "should add pkcs7 padding when length is not a multiple of block size" do
     message = "ATTACK"
-    pkcs7_padding(message, 5).should eql "ATTACK\x04\x04\x04\x04"
+    pkcs7_padding_add(message, 5).should eql "ATTACK\x04\x04\x04\x04"
   end
 
   it "should add pkcs7 padding when length is a multiple of block size" do
     message = "FIGHT"
-    pkcs7_padding(message, 5).should eql "FIGHT\x05\x05\x05\x05\x05"
+    pkcs7_padding_add(message, 5).should eql "FIGHT\x05\x05\x05\x05\x05"
+  end
+
+  it "should remove pkcs7 padding when padding is less than block size" do
+    message = "ABCABC\u0004\u0004\u0004\u0004"
+    pkcs7_padding_remove(message, 5).should eql "ABCABC"
+  end
+
+  it "should remove pkcs7 padding when padding is equal to block size" do
+    message = "ABCA\u0004\u0004\u0004\u0004"
+    pkcs7_padding_remove(message, 4).should eql "ABCA"
   end
 end
