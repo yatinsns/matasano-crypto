d147f8117027e98dd8e9a868ab166c405e5e9319
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-16 15:28:17 +0530

    Fix pkcs7 padding add/remove

--- set2/challenge9/pkcs7.rb
+++ set2/challenge9/pkcs7.rb
@@ -1,10 +1,19 @@
-def pkcs7_padding_add(message, block_size)
-  padding_length = block_size - (message.length % block_size)
-  message.ljust(message.length + padding_length,
-                padding_length.chr)
-end
 
-def pkcs7_padding_remove(message, block_size)
-  padding_length = message[message.length - 1].ord
-  message.slice(0, message.length - padding_length)
+module PKCS7
+  def self.pkcs7_padding_add(message, block_size)
+    padding_length = block_size - (message.length % block_size)
+    message.ljust(message.length + padding_length,
+                  padding_length.chr)
+  end
+
+  def self.pkcs7_padding_remove(message)
+    pkcs7_padding_remove_bytes(message.each_char.map {|chr| chr.ord}).map do |byte|
+      byte.chr
+    end.join
+  end
+
+  def self.pkcs7_padding_remove_bytes(message_bytes)
+    padding_length = message_bytes[message_bytes.length - 1]
+    message_bytes[0...(message_bytes.length - padding_length)]
+  end
 end
--- set2/challenge9/spec/pkcs7_spec.rb
+++ set2/challenge9/spec/pkcs7_spec.rb
@@ -1,23 +1,25 @@
 require_relative "../pkcs7.rb"
 
+include PKCS7
+
 describe "pkcs7" do
   it "should add pkcs7 padding when length is not a multiple of block size" do
     message = "ATTACK"
-    pkcs7_padding_add(message, 5).should eql "ATTACK\x04\x04\x04\x04"
+    PKCS7::pkcs7_padding_add(message, 5).should eql "ATTACK\x04\x04\x04\x04"
   end
 
   it "should add pkcs7 padding when length is a multiple of block size" do
     message = "FIGHT"
-    pkcs7_padding_add(message, 5).should eql "FIGHT\x05\x05\x05\x05\x05"
+    PKCS7::pkcs7_padding_add(message, 5).should eql "FIGHT\x05\x05\x05\x05\x05"
   end
 
   it "should remove pkcs7 padding when padding is less than block size" do
     message = "ABCABC\u0004\u0004\u0004\u0004"
-    pkcs7_padding_remove(message, 5).should eql "ABCABC"
+    PKCS7::pkcs7_padding_remove(message).should eql "ABCABC"
   end
 
   it "should remove pkcs7 padding when padding is equal to block size" do
     message = "ABCA\u0004\u0004\u0004\u0004"
-    pkcs7_padding_remove(message, 4).should eql "ABCA"
+    PKCS7::pkcs7_padding_remove(message).should eql "ABCA"
   end
 end
