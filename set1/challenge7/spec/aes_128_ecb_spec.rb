992343a00d4eba8085b9875477370473a2d538ff
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-22 18:57:47 +0530

    Fix aes-128-ecb tests

--- set1/challenge7/spec/aes_128_ecb_spec.rb
+++ set1/challenge7/spec/aes_128_ecb_spec.rb
@@ -6,6 +6,11 @@ describe "AES" do
     base64_encoding.should eql "vbGE1E4fwdMGCUW1PJlPSA=="
   end
 
+  it "should encrypt more than 128 bits in ECB mode with padding" do
+    base64_encoding = AES.encrypt_128_ecb("Rollin' in my 5.0\nWith my rag-top down so my hair can blow\nThe girlies on standby waving just to say hi\nDid you stop? No, I just drove by\n", "YELLOW SUBMARINE", true)
+    base64_encoding.should eql "cuOUaQATt87kAN3H0MTJO/dJaqRKo4scCm9DIbVxchr56jI2Hsn5oWlqHxwk10I2cYqzfoHCUKTKl6r5XYj1w/XD9UxdEJ2l44Az5MXa+J2ueLxF7bTc9X0vH+mv3pP0zGhvwGCUTrjFQrKhvqwRYMxZTt74816hiqmz4Lv0VWSLH/NmWIHyLS1HENp1nj1V"
+  end
+
   it "should encrypt 128 bits in ECB mode with padding" do
     base64_encoding = AES.encrypt_128_ecb("abcdefghijklmnop", "YELLOW SUBMARINE", true)
     base64_encoding.should eql "vbGE1E4fwdMGCUW1PJlPSGD6NnB+RfSZ26DyW5IjAaU="
