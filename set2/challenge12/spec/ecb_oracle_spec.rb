8f63a64fe38ac96a16f55dcc1a9fef5f89a98c20
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-22 19:14:30 +0530

    Add tests

---
+++ set2/challenge12/spec/ecb_oracle_spec.rb
@@ -0,0 +1,19 @@
+require_relative "../ecb_oracle.rb"
+require_relative "../../challenge9/pkcs7.rb"
+
+EXPECTED_RESULT = "Rollin' in my 5.0
+With my rag-top down so my hair can blow
+The girlies on standby waving just to say hi
+Did you stop? No, I just drove by
+"
+
+describe "ECB Oracle" do
+  it "should find out the suffix" do
+    expect(hack_ecb_oracle).to eql(EXPECTED_RESULT)
+  end
+
+  it "should get correct length for suffix" do
+    expected_length = PKCS7::pkcs7_padding_add(EXPECTED_RESULT, 16).length
+    expect(hack_ecb_oracle_length).to eql(expected_length)
+  end
+end
